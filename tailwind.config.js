1f0f1177d03a8cb82b4cd7b8e746a403697950ce
Lidia Qama <lidia22qama@gmail.com>
2024-10-30 11:53:07 +0100

    adjust search button size

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         customTeal: '#0d9488',
         customGray: '#e5e7eb',
       },
+      screens: {
+        xs: '320px',
+      },
       animation: {
         typing: 'typing 3s steps(20, end) forwards, blink 0.75s step-end infinite',
       },
@@ -21,7 +24,7 @@ module.exports = {
         },
         blink: {
           '0%, 100%': { borderColor: 'transparent' },
-          '50%': { borderColor: 'black' }, // Change this to match your text color
+          '50%': { borderColor: 'black' },
         },
       },
     },
