e752d40237c7f51733d16f0df905e8d1925bcac6
Lidia Qama <lidia22qama@gmail.com>
2024-10-30 08:12:30 +0100

    corrections

--- src/components/posts/PostDetails.tsx
+++ src/components/posts/PostDetails.tsx
@@ -59,7 +59,7 @@ export function PostDetails() {
 
   return (
     <>
-      <div className="text-slate-900 dark:rounded-md dark:bg-gray-900 dark:text-gray-200 mx-auto max-w-7xl px-4 sm:lg:px-6 md:px-3 lg:px-8 mt-10 mb-10">
+      <div className="text-slate-900 dark:rounded-md dark:bg-gray-900 dark:text-gray-200 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-10 mb-10">
         <div className="grid grid-cols-1 lg:grid-cols-2">
           <div className="slider-box w-full h-full max-lg:mx-auto mx-0 mb-5 mt-5">
             <img
@@ -378,7 +378,7 @@ export function PostDetails() {
       </div>
 
       {post.reviews.map((review) => (
-        <div className="mx-auto max-w-4xl px-4 sm:lg:px-6 md:px-3 lg:px-8 text-sm flex flex-col-reverse justify-items-center bg-slate-100 dark:bg-slate-800 rounded-lg p-6 dark:highlight-white/5 shadow-lg mb-10">
+        <div className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 text-sm flex flex-col-reverse justify-items-center bg-slate-100 dark:bg-slate-800 rounded-lg p-6 dark:highlight-white/5 shadow-lg mb-10">
           <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
             <p>{review.comment}</p>
           </blockquote>
@@ -390,11 +390,11 @@ export function PostDetails() {
                 {[...Array(5)].map((_, index) => {
                   const starValue = index + 1;
 
-                  let starClass = 'text-gray-300'; 
+                  let starClass = 'text-gray-300';
                   if (review.rating >= starValue) {
-                    starClass = 'text-yellow-400'; 
+                    starClass = 'text-yellow-400';
                   } else if (review.rating >= starValue - 0.5) {
-                    starClass = 'text-yellow-400 half'; 
+                    starClass = 'text-yellow-400 half';
                   }
 
                   return (
@@ -406,9 +406,8 @@ export function PostDetails() {
                       fill={starClass.includes('half') ? 'url(#half-star)' : 'currentColor'}
                       viewBox="0 0 22 20"
                     >
-                      {/* Defining the path for the star */}
                       <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                      {/* SVG gradient for the half star effect */}
+
                       {starClass.includes('half') && (
                         <defs>
                           <linearGradient id="half-star" x1="0" x2="100%" y1="0" y2="0">
